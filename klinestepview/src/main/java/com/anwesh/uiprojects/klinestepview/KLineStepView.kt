e94eb06e4d15e327114d0635835f5ee1b6ddc9c5
Anwesh43 <anweshthecool0@gmail.com>
2019-02-19 23:46:38 +0530

    adding KLSNode class

--- klinestepview/src/main/java/com/anwesh/uiprojects/klinestepview/KLineStepView.kt
+++ klinestepview/src/main/java/com/anwesh/uiprojects/klinestepview/KLineStepView.kt
@@ -118,4 +118,48 @@ class KLineStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class KLSNode(var i : Int, val state : State = State()) {
+
+        private var prev : KLSNode? = null
+        private var next : KLSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor(){
+            if (i < nodes - 1) {
+                next = KLSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawKLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : KLSNode {
+            var curr : KLSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
