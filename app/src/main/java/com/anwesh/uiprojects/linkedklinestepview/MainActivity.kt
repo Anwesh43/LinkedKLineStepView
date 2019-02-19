3a7c275c4ea32befd360e7e56fe65dd14dd7481e
Anwesh43 <anweshthecool0@gmail.com>
2019-02-19 23:58:26 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedklinestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedklinestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedklinestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.klinestepview.KLineStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        KLineStepView.create(this)
     }
 }
