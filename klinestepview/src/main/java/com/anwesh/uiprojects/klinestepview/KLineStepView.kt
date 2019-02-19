c0f9405e450553b9bd2cffd83dd3e0f9df86ba26
Anwesh43 <anweshthecool0@gmail.com>
2019-02-19 10:34:45 +0530

    adding libraries and constants

--- klinestepview/src/main/java/com/anwesh/uiprojects/klinestepview/KLineStepView.kt
+++ klinestepview/src/main/java/com/anwesh/uiprojects/klinestepview/KLineStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.klinestepview
 /**
  * Created by anweshmishra on 19/02/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#212121")
